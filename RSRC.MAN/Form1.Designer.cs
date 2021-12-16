3683d9927612275cba3b9706939742dc5a0f03ba
mo3ist <50373064+mo3ist@users.noreply.github.com>
2021-12-16 18:44:34 +0200

    feat: Add UI

--- RSRC.MAN/Form1.Designer.cs
+++ RSRC.MAN/Form1.Designer.cs
@@ -28,73 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.create_resource = new System.Windows.Forms.Button();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.create_process = new System.Windows.Forms.Button();
-            this.display_data = new System.Windows.Forms.Button();
-            this.terminate_process = new System.Windows.Forms.Button();
+            this.process_panel = new System.Windows.Forms.Panel();
+            this.resources_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.resources_chart)).BeginInit();
             this.SuspendLayout();
-            // 
-            // create_resource
-            // 
-            this.create_resource.Location = new System.Drawing.Point(168, 151);
-            this.create_resource.Name = "create_resource";
-            this.create_resource.Size = new System.Drawing.Size(139, 48);
-            this.create_resource.TabIndex = 0;
-            this.create_resource.Text = "Create Resource";
-            this.create_resource.UseVisualStyleBackColor = true;
-            this.create_resource.Click += new System.EventHandler(this.create_resource_Click);
             // 
             // create_process
             // 
-            this.create_process.Location = new System.Drawing.Point(324, 151);
+            this.create_process.Location = new System.Drawing.Point(12, 440);
             this.create_process.Name = "create_process";
-            this.create_process.Size = new System.Drawing.Size(139, 48);
-            this.create_process.TabIndex = 1;
+            this.create_process.Size = new System.Drawing.Size(254, 65);
+            this.create_process.TabIndex = 0;
             this.create_process.Text = "Create Process";
             this.create_process.UseVisualStyleBackColor = true;
-            this.create_process.Click += new System.EventHandler(this.button1_Click);
+            this.create_process.Click += new System.EventHandler(this.create_process_Click);
             // 
-            // display_data
+            // process_panel
             // 
-            this.display_data.Location = new System.Drawing.Point(168, 214);
-            this.display_data.Name = "display_data";
-            this.display_data.Size = new System.Drawing.Size(446, 57);
-            this.display_data.TabIndex = 2;
-            this.display_data.Text = "Display Data";
-            this.display_data.UseVisualStyleBackColor = true;
-            this.display_data.Click += new System.EventHandler(this.display_data_Click);
+            this.process_panel.AutoScroll = true;
+            this.process_panel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.process_panel.Location = new System.Drawing.Point(12, 12);
+            this.process_panel.Name = "process_panel";
+            this.process_panel.Size = new System.Drawing.Size(254, 422);
+            this.process_panel.TabIndex = 1;
             // 
-            // terminate_process
+            // resources_chart
             // 
-            this.terminate_process.Location = new System.Drawing.Point(469, 151);
-            this.terminate_process.Name = "terminate_process";
-            this.terminate_process.Size = new System.Drawing.Size(145, 48);
-            this.terminate_process.TabIndex = 3;
-            this.terminate_process.Text = "Terminate Process";
-            this.terminate_process.UseVisualStyleBackColor = true;
-            this.terminate_process.Click += new System.EventHandler(this.terminate_process_Click);
+            chartArea2.Name = "ChartArea1";
+            this.resources_chart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.resources_chart.Legends.Add(legend2);
+            this.resources_chart.Location = new System.Drawing.Point(282, 12);
+            this.resources_chart.Name = "resources_chart";
+            series4.ChartArea = "ChartArea1";
+            series4.CustomProperties = "PointWidth=1";
+            series4.Legend = "Legend1";
+            series4.Name = "RAM";
+            series5.ChartArea = "ChartArea1";
+            series5.CustomProperties = "PointWidth=1";
+            series5.Legend = "Legend1";
+            series5.Name = "Semaphores";
+            series6.ChartArea = "ChartArea1";
+            series6.CustomProperties = "PointWidth=1";
+            series6.Legend = "Legend1";
+            series6.Name = "Interfaces";
+            this.resources_chart.Series.Add(series4);
+            this.resources_chart.Series.Add(series5);
+            this.resources_chart.Series.Add(series6);
+            this.resources_chart.Size = new System.Drawing.Size(933, 300);
+            this.resources_chart.TabIndex = 2;
+            this.resources_chart.Text = "RAM";
+            this.resources_chart.Click += new System.EventHandler(this.ram_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(282, 319);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(933, 186);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.terminate_process);
-            this.Controls.Add(this.display_data);
+            this.ClientSize = new System.Drawing.Size(1227, 517);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.resources_chart);
+            this.Controls.Add(this.process_panel);
             this.Controls.Add(this.create_process);
-            this.Controls.Add(this.create_resource);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.resources_chart)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button create_resource;
         private System.Windows.Forms.Button create_process;
-        private System.Windows.Forms.Button display_data;
-        private System.Windows.Forms.Button terminate_process;
+        private System.Windows.Forms.Panel process_panel;
+        private System.Windows.Forms.DataVisualization.Charting.Chart resources_chart;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- RSRC.MAN/Form1.cs
+++ RSRC.MAN/Form1.cs
@@ -12,49 +12,76 @@ namespace RSRC.MAN
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void create_resource_Click(object sender, EventArgs e)
+        private void create_process_Click(object sender, EventArgs e)
         {
-            Resource r = new Resource();
-            Console.WriteLine("New resource created. Resource count: " + Resource.Count);
+            addProcess();
+           // addProcess();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void addProcess()
         {
             Process p = new Process();
-            Console.WriteLine("New process created. Process count: " + Process.Count);
+
+            Label p_text = new Label();
+            p_text.Name = "p_text" + Process.Count;
+            p_text.Text = p_text.Name;
+            p_text.Top = Process.Count * 60;
+            p_text.Left = process_panel.Left;
+
+            Button terminate_process = new Button();
+            terminate_process.Name = "terminate_process" + Process.Count;
+            terminate_process.Text = "Terminate";
+            terminate_process.Top = Process.Count * 60;
+            terminate_process.Left = p_text.Right;
+            terminate_process.Click += (object sender, EventArgs e) =>
+            {
+                process_panel.Controls.Remove(p_text);
+                process_panel.Controls.Remove(terminate_process);
+                p.Terminate();
+            };
+
+            process_panel.Controls.Add(p_text);
+            process_panel.Controls.Add(terminate_process);
+
         }
 
-        private void display_data_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            // Available
-            Console.WriteLine("Available:");
-            foreach (int a in Resource.AvailableVector)
-            {
-                Console.Write("{0} ", a);
-            }
-
-            Console.WriteLine();
-
-            // Allocated
-            Console.WriteLine("Allocated:");
-            foreach (List<int> vec in Process.AllocatedMatrix)
-            {
-                foreach (int a in vec)
-                {
-                    Console.Write("{0} ", a);
-                }
-                Console.WriteLine();
-            }
+            Resource r1 = new Resource();
+            Resource r2 = new Resource();
+            Resource r3 = new Resource();
+            //fillResourceChars();
         }
 
-        private void terminate_process_Click(object sender, EventArgs e)
+        private void fillResourceChars()
         {
-            Process.Processes[0].Terminate();
+
+            resources_chart.Series["RAM"].Points.Clear();
+            resources_chart.Series["Semaphores"].Points.Clear();
+            resources_chart.Series["Interfaces"].Points.Clear();
+            resources_chart.Series["RAM"].Points.AddXY("Process 1", "10000");
+            resources_chart.Series["Semaphores"].Points.AddXY("Process 1", "1000");
+        }
+
+        private void ram_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            fillResourceChars();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Console.WriteLine("hi");
         }
     }
 }
